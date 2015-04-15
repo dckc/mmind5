3105bf548acddeca20bc189268de7560c1d5dfc2
Dan Connolly <http://www.madmode.com/>
2015-04-14 23:37:55 -0500

    guess_score compiles

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@
 extern crate rand;
 
 use std::cmp::Ordering;
-use std::iter::FromIterator;
+use std::hash::{Hash, Hasher};
+use std::iter::{FromIterator};
 use std::fmt::{self, Debug, Formatter};
-use std::collections::{BitSet, BitVec};
+use std::collections::{BitSet, BitVec, HashMap};
 use rand::{Rand, Rng};
 
 #[derive_Rand]
@@ -30,6 +31,20 @@ struct Distance {
     blacks: usize,
     whites: usize
 }
+
+impl Distance {
+    fn win(self) -> bool {
+        self.blacks == Pattern::size()
+    }
+}
+
+impl Hash for Distance {
+    fn hash<H>(&self, state: &mut H) 
+        where H: Hasher {
+        (self.blacks, self.whites).hash(state)
+    }
+}
+
 
 impl Ord for Distance {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -154,48 +169,60 @@ impl Rand for Pattern {
 }
 
 
-#[derive(Debug)]
-struct Solver {
+struct Solver<'a> {
+    codemaker: &'a Fn(&Pattern) -> Distance,
+    guessed: Vec<Pattern>,
     s: BitSet,
-    guess: Pattern
 }
 
 // http://en.wikipedia.org/wiki/Mastermind_%28board_game%29#Five-guess_algorithm
-impl Solver {
-    fn new() -> Solver {
+impl<'a> Solver<'a> {
+    fn new(codemaker: &'a Fn(&Pattern) -> Distance) -> Solver<'a> {
         // 1. Create the set S of 1296 possible codes, 1111,1112,.., 6666.
         let possible_codes = BitSet::from_bit_vec(
             BitVec::from_fn(Pattern::cardinality() as usize, |_| true));
         // 2. Start with initial guess 1122
         let initial_guess = [Red, Red, Orn, Orn];
-        Solver { s: possible_codes, guess: Pattern::new(initial_guess) }
+        Solver { codemaker: codemaker,
+                 s: possible_codes,
+                 guessed: vec![Pattern::new(initial_guess)] }
     }
 
     // Return Some(winning_pattern) or None if we need another turn.
-    fn play(self: &mut Self, peg_score: &Fn(&Pattern) -> Distance) -> Option<Pattern> {
+    fn play(self: &mut Self) -> Option<Pattern> {
         // 3. Play the guess to get a response of colored and white pegs.
-        let response = peg_score(&self.guess);
+        let guess = self.guessed[self.guessed.len() - 1];
+        let d = (self.codemaker)(&guess);
 
         // If the response is four colored pegs, the game is won, the algorithm terminates.
-        if response.blacks == Pattern::size() {
-            Some(self.guess)
+        if d.win() {
+            Some(guess)
         } else {
             // 5. Otherwise, remove from S any code that would not
             // give the same response if it (the guess) were the code.
-            self.remove_mismatches(peg_score, response);
+            self.remove_mismatches(d);
 
-            // 6. Apply minimax technique ... TODO
+            // TODO:
+    // From the set of guesses with the maximum score, select one as
+    // the next guess, choosing a member of S whenever
+    // possible. (Knuth follows the convention of choosing the guess
+    // with the least numeric value e.g. 2345 is lower than
+    // 3456. Knuth also gives an example showing that in some cases no
+    // member of S will be among the highest scoring guesses and thus
+    // the guess cannot win on the next turn, yet will be necessary to
+    // assure a win in five.)
+            self.choose_guess();
 
             None
         }
     }
 
-    fn remove_mismatches(self: &mut Self, peg_score: &Fn(&Pattern) -> Distance, response: Distance) {
-        // 5. Otherwise, remove from S any code that would not
-        // give the same response if it (the guess) were the code.
+    // 5. Otherwise, remove from S any code that would not
+    // give the same response if it (the guess) were the code.
+    fn remove_mismatches(self: &mut Self, d: Distance) {
         let to_keep = {
             let same_response = || {
-                self.s.iter().filter(|g| peg_score(&Pattern::ith(*g)) == response)
+                self.s.iter().filter(|g| (self.codemaker)(&Pattern::ith(*g)) == d)
             };
             BitSet::from_iter(same_response())
         };
@@ -203,6 +230,56 @@ impl Solver {
         self.s.intersect_with(&to_keep);
     }
 
+    // For each possible guess, that is, any unused code of the 1296
+    // not just those in S, calculate how many possibilities in S
+    // would be eliminated for each possible colored/white peg score.
+    // The score of a guess is the minimum number of possibilities it
+    // might eliminate from S. A single pass through S for each unused
+    // code of the 1296 will provide a hit count for each
+    // colored/white peg score found; the colored/white peg score with
+    // the highest hit count will eliminate the fewest possibilities;
+    fn unused_guess_scores(self: &Self)
+        -> HashMap<usize, Vec<Pattern>>
+    {
+        // TODO: figure out how to move iter_d to Distance::iter
+        let qty = Pattern::size();
+        let iter_d = || {
+            (0..qty+1)
+                .flat_map(|blacks: usize| {
+                    (0..qty+1)
+                        .filter(move |whites| blacks + whites <= qty)
+                        .map(move |whites: usize| Distance { blacks: blacks, whites: whites })
+                })
+        };
+
+        // calculate the score of a guess by using "minimum eliminated" =
+        // "count of elements in S" - (minus) "highest hit count".
+        let guess_score = |gix| {
+            let mut dist_by_hits = HashMap::new();
+            for other_ix in self.s.iter() {
+                let g = Pattern::ith(gix);
+                let other = Pattern::ith(other_ix);
+                let d = g.score(other);
+                *(dist_by_hits.entry(d).or_insert(0)) += 1;
+            }
+
+            let highest_hit_count = dist_by_hits.values().max().unwrap();
+            self.s.len() - highest_hit_count
+        };
+
+        let mut guesses_with_score = HashMap::new();
+        for guess_ix in 0..Pattern::cardinality() {
+            if !self.guessed.contains(&Pattern::ith(guess_ix)) {
+                let score = guess_score(guess_ix);
+                let guess = Pattern::ith(guess_ix);
+                guesses_with_score.entry(score).or_insert(vec![]).push(guess)
+            }
+        }
+        guesses_with_score
+    }
+
+    fn choose_guess(self: &mut Self) {
+    }
 }
 
 
@@ -218,7 +295,9 @@ fn main() {
         println!("...........feedback: {:?}", secret.score(guess));
     }
 
-    let mut breaker = Solver::new();
-    breaker.play(&|g| secret.score(*g));
-    println!("Five guess codebreaker: {:?}", breaker);
+    let maker = |guess: &Pattern| secret.score(*guess);
+
+    let mut breaker = Solver::new(&maker);
+    breaker.play();
+    println!("Five guess codebreaker: {:?}", breaker.guessed);
 }
